9c8caecc456b8213190b7f76d8b5a235a614441c
gustavomes <gustavomes@yahoo.com.br>
2021-11-04 14:12:03 -0300

    novos inputs

--- js/script.js
+++ js/script.js
@@ -7,7 +7,13 @@ let campo3 = document.getElementById("imgUrl");
 let logo = document.querySelector('#logo');
 let botao = document.getElementById("btn")
 let DivImage = document.getElementById ("divImg");
-let list = document.querySelector('ol');
+let list = document.querySelector('ol');  
+let cond = document.querySelector('input[type=\'checkbox\']');
+
+let par = document.querySelector('p');
+
+// pegando todos os inputs que são type=radio
+let rotas = document.querySelectorAll('input[type=\'radio\']');
 
 //evento on mouse
 
@@ -29,7 +35,23 @@ botao.addEventListener('click', (event) => {
   // impede que o evento padrão aconteça (no caso do form, não envia para um backend)
   event.preventDefault(); 
   
+  let rota;
+  for (let i = 0; i < rotas.length; i++) {
+    if (rotas[i].checked) {
+      rota = rotas[i].value;
+      break;
+    }
+  }
 
+  let condTxt;
+  cond.checked ? condTxt = 'pode mundar de rota' : condTxt = 'nunca muda de rota';
+
+  // Mensagem: Matricula efetivada com sucesso!
+  // Nome: ??? Sobrenome: ??? Periodo: ??? Condições: Aceitou/Não Aceitou
+  par.innerText = `O campeão ${field1.value}, sempre usa 
+  ${field2.value}, pela ${rota} lane, e  ${condTxt}.`
+  
+  
   //Criação dos Elementos
 
   let div = document.createElement("div");
@@ -39,7 +61,7 @@ botao.addEventListener('click', (event) => {
 
   //localização dos elementos
   main.appendChild(div) 
-  list.appendChild(li);
+  list.appendChild(par);
   DivImage.appendChild(img);
 
 
